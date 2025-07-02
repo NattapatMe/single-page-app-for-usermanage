29b70da15d8e5e28827a023df9e3463d5fe2ffdf
Famai <famai.ntp@gmail.com>
2025-07-02 14:56:45 +0700

    bind form on login page with login button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -28,7 +28,7 @@
     <h1 class="text-2xl font-bold mb-6">Please Login</h1>
 
     {#if !isLoggedIn}
-        <form class="space-y-4">
+        <form on:submit|preventDefault={login} class="space-y-4">
             <input bind:value={username} placeholder="Username" class="w-full p-2 border rounded" />
             <input bind:value={password} type="password" placeholder="Password" class="w-full p-2 border rounded" />
             <button type="submit" class="w-full bg-blue-500 text-white p-2 rounded">Login</button>
