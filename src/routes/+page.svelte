b47fb52885ca1a59ac5a68cfe95efdce55112bcb
Famai <famai.ntp@gmail.com>
2025-07-02 14:39:11 +0700

    bind inputs to username and password variables

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,14 @@
+<script>
+  let username = '';
+  let password = '';
+</script>
+
 <div class="flex flex-col items-center justify-start pt-10">
     <h1 class="text-2xl font-bold mb-6">Please Login</h1>
 
     <form class="space-y-4">
-        <input placeholder="Username" class="w-full p-2 border rounded" />
-        <input type="password" placeholder="Password" class="w-full p-2 border rounded" />
+        <input bind:value={username} placeholder="Username" class="w-full p-2 border rounded" />
+        <input bind:value={password} type="password" placeholder="Password" class="w-full p-2 border rounded" />
         <button type="submit" class="w-full bg-blue-500 text-white p-2 rounded">Login</button>
     </form>
 </div>
