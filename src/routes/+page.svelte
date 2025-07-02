30a3c377783fd2e7c2a49ca4038064942e933264
Famai <famai.ntp@gmail.com>
2025-07-02 14:53:01 +0700

    add logout and go to users button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -27,9 +27,17 @@
 <div class="flex flex-col items-center justify-start pt-10">
     <h1 class="text-2xl font-bold mb-6">Please Login</h1>
 
-    <form class="space-y-4">
-        <input bind:value={username} placeholder="Username" class="w-full p-2 border rounded" />
-        <input bind:value={password} type="password" placeholder="Password" class="w-full p-2 border rounded" />
-        <button type="submit" class="w-full bg-blue-500 text-white p-2 rounded">Login</button>
-    </form>
+    {#if !isLoggedIn}
+        <form class="space-y-4">
+            <input bind:value={username} placeholder="Username" class="w-full p-2 border rounded" />
+            <input bind:value={password} type="password" placeholder="Password" class="w-full p-2 border rounded" />
+            <button type="submit" class="w-full bg-blue-500 text-white p-2 rounded">Login</button>
+        </form>
+    {:else}
+        <p class="mb-4 text-green-700">You are already logged in</p>
+        <div class="space-x-2">
+            <button on:click={goToUsers} class="bg-blue-500 text-white px-4 py-2 rounded">View Users</button>
+            <button on:click={logout} class="bg-red-500 text-white px-4 py-2 rounded">Logout</button>
+        </div>
+    {/if}
 </div>
