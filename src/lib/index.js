b5b3dd9c8526d7c80c95989daa69540847dbc025
Famai <famai.ntp@gmail.com>
2025-07-02 14:45:19 +0700

    create auth store

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,9 @@
-// place files you want to import through the `$lib` alias in this folder.
+import { writable } from 'svelte/store';
+
+// --- Auth Store ---
+const storedAuth = localStorage.getItem('auth');
+const initialAuth = storedAuth ? JSON.parse(storedAuth) : { loggedIn: false };
+export const auth = writable(initialAuth);
+auth.subscribe(value => {
+  localStorage.setItem('auth', JSON.stringify(value));
+});
